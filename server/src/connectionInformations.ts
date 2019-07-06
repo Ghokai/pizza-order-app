ca7d3f0b9f9814493fc10bf66dcdc23139e7b3d0
Gokay Arpaci <gokayarpaci@gmail.com>
2019-07-06 23:08:28 +0300

    container db host

--- server/src/connectionInformations.ts
+++ server/src/connectionInformations.ts
@@ -1,5 +1,6 @@
 export const dbType = "postgres";
 export const dbHost = "localhost";
+export const dbHostContainer = "postgres";
 export const dbPort = 5432;
 export const dbUser = "postgres";
 export const dbPassword = "password";
